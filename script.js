0385850edd24086a0300a46a5ae328ff7162f583
iyappan22 <78902343+iyappan22@users.noreply.github.com>
2021-03-08 00:55:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+var data = fetch("https://restcountries.eu/rest/v2/all");
+
+data
+  .then( (response)=> {
+    return response.json();
+  })
+  .then((response)=> {
+    restcountries(response)
+    console.log(response)
+})
+  .catch(function (err) {
+    console.log(err);
+  });
+
+function restcountries(response){
+for(i in response){
+    createdata(i,response)
+}
+ 
+}
+
+var container = document.createElement("div");
+container.setAttribute("class", "container bg");
+
+var row = document.createElement("div");
+row.setAttribute("class", "row");
+
+function createdata(i,data){
+
+  
+
+var col = document.createElement("div");
+col.setAttribute("class", "col-lg-4 card shadow");
+
+var header = document.createElement("div");
+header.setAttribute("class", "card-header");
+header.innerText = data[i].name;
+
+var body = document.createElement("div");
+body.setAttribute("class", "card-body");
+
+var image = document.createElement("img");
+
+image.src = data[i].flag;
+
+var capital = document.createElement("div");
+
+capital.innerText = "Capital :"+" "+ data[i].capital;
+
+
+var region = document.createElement("div");
+
+region.innerText = "Region :"+" " + data[i].region;
+
+var countrycode = document.createElement("div");
+
+countrycode.innerText ="Country Code :"+" "+ data[i].alpha3Code;
+
+var button =  document.createElement("button");
+var weathercountry = data[i].capital;
+button.setAttribute("class", "btn btn-primary");
+button.innerText = "Click for Weather";
+button.addEventListener('click',function(){
+    var result = fetch('https://api.openweathermap.org/data/2.5/weather?q='+weathercountry+'&appid=450767ab95b7a0a89f79899717edcde5')
+
+result
+.then( (weatherresult)=> {
+    return weatherresult.json();
+  })
+  .then((weatherresult)=> {
+    var namecountry =  data[i].name
+    var tempvalue = weatherresult['main']['temp']
+    var lonvalue = weatherresult['coord']['lon']
+    var lanvalue = weatherresult['coord']['lat']
+    alert('Name:'+' '+namecountry+'\n'+'Temperature:'+' '+tempvalue+'\n'+'Longitude:'+' '+lonvalue+'\n'+'Latitude:'+' '+lanvalue )
+})
+  .catch(function (err) {
+    alert("err0r")
+  });
+
+
+});
+
+body.append(image, capital, region, countrycode,button);
+col.append(header, body );
+
+row.append(col);
+
+container.append(row);
+
+document.body.append(container);
+
+
+}
+
+
